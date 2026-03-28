c59e67dd657e42fd618496b6206d48d88f47de06
BikashShah-404 <shahbikash4659@gmail.com>
2026-03-28 18:32:49 +0545

    serverless didn't worked

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
-import serverless from "serverless-http";
 import app from "../app.js";
 import connectDB from "../db/index.js";
 
